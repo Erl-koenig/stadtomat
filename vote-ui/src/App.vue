2e4dafff22b69d36055f273a7224154c8a3fe6ad
David Stäheli <mistrdave@gmail.com>
2024-09-23 21:37:50 +0200

    first sample with Category :rocket:

--- vote-ui/src/App.vue
+++ vote-ui/src/App.vue
@@ -1,5 +1,31 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
+import supabase from './services/supabase.ts'
+
+interface Category {
+  id: string
+  title: string
+  description: string
+  created_at: string
+}
+
+const categories = ref<Category[]>([]);
+
+async function getCategories() {
+  let { data, error } = await supabase
+    .from('category')
+    .select('*')
+  if (error) {
+    console.log(error)
+  } else {
+    categories.value = data as Category[]
+  }
+}
+
+onMounted(() => {
+  getCategories()
+})
 </script>
 
 <template>
@@ -11,7 +37,11 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h2>Categories</h2>
+  <button type="button" @click="getCategories">Refresh</button>
+  <ul>
+    <li v-for="category in categories" :key="category.id"><b>{{ category.title }}:</b> {{ category.description }}</li>
+  </ul>
 </template>
 
 <style scoped>
@@ -21,9 +51,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
