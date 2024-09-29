9eed756b6ac0d452ecde057d1e09798909ffbd12
Erl-koenig <knoefler.ivan@gmail.com>
2024-09-29 15:10:01 +0200

    add new item component

--- vote-ui/src/pages/AddItem.vue
+++ vote-ui/src/pages/AddItem.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <h1>Add Item Page</h1>
+    <NewItemForm />
   </div>
 </template>
 
 <script lang="ts">
+import NewItemForm from '../components/NewItemForm.vue';
+
 export default {
   name: 'additem',
+  components: {
+    NewItemForm,
+  },
 };
 </script>
