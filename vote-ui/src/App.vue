a4c9f870bbf0822098205050a27b02148f0df536
Ivan Knoefler <68387388+Erl-koenig@users.noreply.github.com>
2024-09-29 16:22:02 +0200

    left-sided navbar

--- vote-ui/src/App.vue
+++ vote-ui/src/App.vue
@@ -1,20 +1,22 @@
 <template>
-  <div>
-    <nav>
+  <div id="app">
+    <nav class="vertical-navbar">
       <a href="#" @click.prevent>
         <img src="./assets/logo.png" class="logo" />
       </a>
       <ul>
         <li><router-link to="/">Home</router-link></li>
         <li><router-link to="/additem">Add Item</router-link></li>
-        <li><router-link to="/allitems">AllItems</router-link></li>
+        <li><router-link to="/allitems">All Items</router-link></li>
         <li><router-link to="/votes">Votes</router-link></li>
       </ul>
     </nav>
-
-    <router-view />
+    <div class="content">
+      <router-view />
+    </div>
   </div>
 </template>
+
 
 <script lang="ts">
 export default {
@@ -23,6 +25,38 @@ export default {
 </script>
 
 <style scoped>
+#app {
+  display: flex;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+}
+
+.vertical-navbar {
+  width: 200px;
+  padding-right: 2rem;
+  background-color: #1a1a1a;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+
+.vertical-navbar ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.vertical-navbar li {
+  margin: 15px 0;
+}
+
+.content {
+  margin-left: 220px;
+  padding: 2rem;
+  flex-grow: 1;
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -34,9 +68,13 @@ export default {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
 
-nav ul {
-  list-style-type: none;
-  display: flex;
-  gap: 10px;
+a {
+  font-weight: 500;
+  color: #646cff;
+  text-decoration: inherit;
+}
+
+a:hover {
+  color: #535bf2;
 }
 </style>
