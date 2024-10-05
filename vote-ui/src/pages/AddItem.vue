b8ce81d186bd0498331db2a240af77d1a599c934
Erl-koenig <knoefler.ivan@gmail.com>
2024-10-05 16:45:09 +0200

    add new item using shadcn

--- vote-ui/src/pages/AddItem.vue
+++ vote-ui/src/pages/AddItem.vue
@@ -1,12 +1,10 @@
 <template>
-  <div>
-    <h1>Add Item Page</h1>
+  <div class="flex flex-col items-center mt-10">
+    <h1 class="mb-4 font-bold	text-3xl">Add Item Page</h1>
     <NewItemForm />
   </div>
 </template>
 
 <script setup lang="ts">
 import NewItemForm from '../components/NewItemForm.vue';
-
-
 </script>
