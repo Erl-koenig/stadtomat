3d1faf8cb01943ffeb83a01f3a41004a3f1944ff
Erl-koenig <knoefler.ivan@gmail.com>
2024-10-06 13:36:47 +0200

    votes page added shadcn data table

--- vote-ui/src/pages/AllItems.vue
+++ vote-ui/src/pages/AllItems.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
     <h1>All items Page</h1>
-    <ListItems />
   </div>
 </template>
 
 <script setup lang="ts">
-import ListItems from '../components/ListItems.vue';
 
 
 </script>
