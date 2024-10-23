c0dda8d3bf466f1dfca1745ba074d71a50aae9dd
David Stäheli <mistrdave@gmail.com>
2024-10-23 13:14:36 +0200

    start admin view

--- vote-ui/src/pages/Admin.vue
+++ vote-ui/src/pages/Admin.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import AdminList from '@/components/AdminList.vue';
+
 
 </script>
 <template>
     <div class="flex flex-col items-center">
         <h1 class="text-4xl font-bold text-dark">Admin</h1>
         <p class="text-dark">Hi Admin, you're able to edit the items.</p>
+        <AdminList></AdminList>
     </div>
     <div>
         
