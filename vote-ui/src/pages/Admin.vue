c372ff5edb1bec89788dede4d9ad6321c28a5bca
David Stäheli <mistrdave@gmail.com>
2024-10-24 00:07:24 +0200

    update font

--- vote-ui/src/pages/Admin.vue
+++ vote-ui/src/pages/Admin.vue
@@ -5,7 +5,7 @@ import AdminList from '@/components/AdminList.vue';
 </script>
 <template>
     <div class="flex flex-col items-center">
-        <h1 class="text-4xl font-bold text-dark">Admin</h1>
+        <h1 class="text-4xl font-bold">Admin</h1>
         <p class="text-dark">Hi Admin, you're able to edit the items.</p>
         <AdminList></AdminList>
     </div>
